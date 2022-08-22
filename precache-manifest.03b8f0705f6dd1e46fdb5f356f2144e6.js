349365df6f4731ef00c59f6a0af75dc81833ff7c
ishikadesai <ishidesai2002@gmail.com>
2022-08-22 14:12:49 +0530

    Updates

---
+++ precache-manifest.03b8f0705f6dd1e46fdb5f356f2144e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b38b8dbe1448c91df5a9d9f45a9b3feb",
+    "url": "/airbnb-clone/index.html"
+  },
+  {
+    "revision": "2a1ac00c8d62e5352500",
+    "url": "/airbnb-clone/static/css/main.ba1636ae.chunk.css"
+  },
+  {
+    "revision": "eeeabf953394b8f33413",
+    "url": "/airbnb-clone/static/js/2.3d50facc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/airbnb-clone/static/js/2.3d50facc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1ac00c8d62e5352500",
+    "url": "/airbnb-clone/static/js/main.b3d108e3.chunk.js"
+  },
+  {
+    "revision": "f358a9c4d5787a3a04f2",
+    "url": "/airbnb-clone/static/js/runtime-main.fc586be8.js"
+  }
+]);
